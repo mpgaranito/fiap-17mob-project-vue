dc95eb1143925492e457fa9053393a58662539ee
Marcos Garanito <mpgaranito@gmail.com>
2019-05-25 18:29:41 -0300

    primeiro commit

---
+++ dashboard/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+   <b-alert show>Default Alert</b-alert>
+  </div>
+</template>
+
+ 
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
